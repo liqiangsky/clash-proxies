361377486269b16bed888124a7b851580b24fccc
liqiangsky <74276685+liqiangsky@users.noreply.github.com>
2026-04-07 17:57:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ URLS = [
     #"https://raw.githubusercontent.com/shaoyouvip/free/refs/heads/main/all.yaml",
     #"https://ghfast.top/https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/c.yaml",
     #"https://vahid.ehsandigik.ir/clash",
-    "https://dy.reiasu.jp",
+    "https://ghfast.top/https://dy.reiasu.jp",
     #"https://jd.zhujunlong.eu.org",
     #"https://proxy.525168.xyz"
 ]
